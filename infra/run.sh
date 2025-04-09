9aaef0650530e7260eff74a9be70266ea25020a2
WALRUS911 <tok911@ya.ru>
2025-04-09 21:57:32 +0300

    rev_1.13

--- infra/run.sh
+++ infra/run.sh
@@ -6,6 +6,7 @@ docker-compose -f docker-compose.yml ps
 docker-compose -f docker-compose.yml exec backend python manage.py makemigrations recipes
 docker-compose -f docker-compose.yml exec backend python manage.py migrate
 docker-compose -f docker-compose.yml exec backend python manage.py collectstatic
+docker-compose -f docker-compose.yml exec backend cp -r /app/collected_static/. /static/
 docker-compose -f docker-compose.yml exec backend cp -r /app/collected_static/. /admin_static/
 docker-compose -f docker-compose.yml exec -it backend python manage.py createsuperuser
 docker-compose -f docker-compose.yml exec backend python manage.py import_ingredients
