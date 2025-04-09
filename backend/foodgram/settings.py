03e11c95035dbe3f9fc548d3b96b67fac5aaffac
WALRUS911 <tok911@ya.ru>
2025-04-09 19:24:03 +0300

    rev_1.9

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -7,9 +7,9 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
+# ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 SECRET_KEY = os.getenv('SECRET_KEY', default='django-token')
-# ALLOWED_HOSTS =['*']
+ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
